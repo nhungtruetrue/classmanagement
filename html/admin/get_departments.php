cf08defca4c4dbfaa129a2fc22141d2c02d67ae5
truetrue <dthnhung12k25@gmail.com>
2025-05-21 21:44:14 +0700

    hello

--- html/admin/get_departments.php
+++ html/admin/get_departments.php
@@ -4,10 +4,10 @@ include('../connect.php');
 $sql_departments = "SELECT id, name FROM departments";
 $result_departments = $conn->query($sql_departments);
 $departments = [];
-
+//
 if ($result_departments && $result_departments->num_rows > 0) {
     while ($row = $result_departments->fetch_assoc()) {
         $departments[] = $row;
     }
 }
-?>
+?> 
